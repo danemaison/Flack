24d1d48a1deb2b19a9522870c623ad1f915cdbcb
Dane Maison <33366146+danemaison@users.noreply.github.com>
2019-09-14 21:47:31 -0700

    refactor login.js

--- static/login.js
+++ static/login.js
@@ -12,21 +12,21 @@ document.addEventListener('DOMContentLoaded', function(){
       console.log('hello');
       var username = document.querySelector('.username').value;
       var allowed = true;
-      if (username.match(/^[0-9a-zA-Z]{1,16}$/)){
-        allowed = true;
-      }
-      else{
-        window.alert("Please do not use spaces or symbols in your username")
-        allowed = false;
-        document.querySelector('.username').value = "";
-        return false;
-      }
+//       if (username.match(/^[0-9a-zA-Z]{1,16}$/)){
+//         allowed = true;
+//       }
+//       else{
+//         window.alert("Please do not use spaces or symbols in your username")
+//         allowed = false;
+//         document.querySelector('.username').value = "";
+//         return false;
+//       }
 
-      if(username.length > 11){
-        window.alert("Please keep your display name under 12 characters")
-        allowed = false;
-        return false
-      }
+//       if(username.length > 11){
+//         window.alert("Please keep your display name under 12 characters")
+//         allowed = false;
+//         return false
+//       }
 
       if (allowed == true){
         localStorage.setItem('username',`${username}`);
