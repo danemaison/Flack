79d883a419ea814ad9c2e2c16cdfa6e21207219f
Dane Maison <danemaison@gmail.com>
2019-10-29 20:21:20 -0700

    fixed typo on login init function

--- static/login.js
+++ static/login.js
@@ -1,28 +1,28 @@
 $(document).ready(init)
 
-function int(){
-  $('.form').on('submit', function(event){
-    return false;  
+function init(){
+  $('.form').on('submit', function(){
+    return false;
   });
-  
+
   $('.join').on('click', function(){
       var username = document.querySelector('.username').value;
       var allowed = true;
-//       if (username.match(/^[0-9a-zA-Z]{1,16}$/)){
-//         allowed = true;
-//       }
-//       else{
-//         window.alert("Please do not use spaces or symbols in your username")
-//         allowed = false;
-//         document.querySelector('.username').value = "";
-//         return false;
-//       }
+      if (username.match(/^[0-9a-zA-Z]{1,16}$/)){
+        allowed = true;
+      }
+      else{
+        window.alert("Please do not use spaces or symbols in your username")
+        allowed = false;
+        document.querySelector('.username').value = "";
+        return false;
+      }
 
-//       if(username.length > 11){
-//         window.alert("Please keep your display name under 12 characters")
-//         allowed = false;
-//         return false
-//       }
+      if(username.length > 11){
+        window.alert("Please keep your display name under 12 characters")
+        allowed = false;
+        return false
+      }
 
       if (allowed == true){
         localStorage.setItem('username',`${username}`);
@@ -35,5 +35,3 @@ function int(){
   });
 
 }
-
-
